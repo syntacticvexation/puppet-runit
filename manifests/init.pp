1346da8e94f946ab103e202b542e71965b48fdd5
Syntactic Vexation <syntacticvexation@gmail.com>
2017-08-16 16:19:22 +1000

    Simplified init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,13 @@
 class runit( $ensure = present ) {
-
-  package { runit: ensure => $ensure }
-
-  if $ensure == present {
-    file {
-      '/etc/sv':
-        ensure => directory,
-        ;
-      '/etc/service':
-        ensure => directory,
-        ;
-    }
-
+  package { runit:
+    ensure => $ensure
   }
 
+  if $ensure == present {
+    $dirs = ['/etc/sv','/etc/service']
+
+    file { $dirs:
+        ensure => directory,
+    }
+  }
 }
